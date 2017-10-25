8cb9ddf9dd2485e51bae807d6dff5ea7d3741109
Shaivya Paliwal <32912499+shaivyapaliwal@users.noreply.github.com>
2017-10-26 00:52:28 +0530

    Add files via upload
    "BLA"

---
+++ Email_Send.py
@@ -0,0 +1,13 @@
+import smtplib
+
+ss = smtplib.SMTP("smtp.gmail.com", 587)
+ss.ehlo()
+ss.starttls()
+ss.login("shaivyapaliwal@gmail.com", "minionslovebapples2409")
+
+msg="Subject: Yello.\nTest mail"
+to:["shaivyapaliwal@gmail.com", "shaivya_bt2k16@dtu.ac.in", "samarthgupta1011@gmail.com"]
+for i in to:
+
+    ss.sendmail("shaivyapaliwal@gmail.com" ,to ,msg)
+ss.close()
