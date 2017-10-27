1c70db298f56f6f6505e06dde4deddf97c8abc5e
Shaivya Paliwal <32912499+shaivyapaliwal@users.noreply.github.com>
2017-10-27 23:30:44 +0530

    Update Email_Send.py

--- Email_Send.py
+++ Email_Send.py
@@ -3,10 +3,10 @@ import smtplib
 ss = smtplib.SMTP("smtp.gmail.com", 587)
 ss.ehlo()
 ss.starttls()
-ss.login("shaivyapaliwal@gmail.com", "minionslovebapples2409")
+ss.login("EMAIL", "PASSWORD")
 
 msg="Subject: Yello.\nTest mail"
-to:["shaivyapaliwal@gmail.com", "shaivya_bt2k16@dtu.ac.in", "samarthgupta1011@gmail.com"]
+to:["SENDER", "SENDER", "SENDER"]
 for i in to:
 
     ss.sendmail("shaivyapaliwal@gmail.com" ,to ,msg)
